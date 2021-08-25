9ad0b529f504aa06d085969b4819fbf24f47d468
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-25 16:31:52 -0500

    update AntiFrameEdit.php

--- src/JonyGamesYT9/AntiFrameEdit/AntiFrameEdit.php
+++ src/JonyGamesYT9/AntiFrameEdit/AntiFrameEdit.php
@@ -9,46 +9,48 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\utils\Config;
 
 /**
- * Class AntiFrameEdit
- * @package JonyGamesYT9\AntiFrameEdit\AntiFrameEdit
- */
-class AntiFrameEdit extends PluginBase implements Listener 
+* Class AntiFrameEdit
+* @package JonyGamesYT9\AntiFrameEdit\AntiFrameEdit
+*/
+class AntiFrameEdit extends PluginBase implements Listener
 {
-  
+
   /** @var Config $config */
   private $config;
-  
+
   /**
-   * @return void 
-   */
-  public function onEnable(): void 
+  * @return void
+  */
+  public function onEnable(): void
   {
-    $this->saveResource("config.yml");
-    $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+    $this->saveResource("messages.yml");
+    $this->config = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
   }
-  
+
   /**
-   * @param PlayerInteractEvent $event
-   * @return void 
-   */
-  public function onInteractFrame(PlayerInteractEvent $event): void 
+  * @param PlayerInteractEvent $event
+  * @return void
+  */
+  public function onInteractFrame(PlayerInteractEvent $event): void
   {
     $player = $event->getPlayer();
     $block = $event->getBlock();
     $action = $event->getAction();
-    if ($action === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
-      if ($player->hasPermission("antiframeedit.place.bypass") or $player->isOp()) {
-        return;
+    if ($block->getId() === Block::ITEM_FRAME_BLOCK) {
+      if ($action === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
+        if ($player->hasPermission("antiframeedit.place.bypass") or $player->isOp()) {
+          return;
+        }
+        $event->setCancelled(true);
+        $player->sendMessage(str_replace(["&"], ["§"], $this->config->get("no.place.item.frame")));
+      } else if ($action === PlayerInteractEvent::LEFT_CLICK_BLOCK) {
+        if ($player->hasPermission("antiframeedit.remove.bypass") or $player->isOp()) {
+          return;
+        }
+        $event->setCancelled(true);
+        $player->sendMessage(str_replace(["&"], ["§"], $this->config->get("no.remove.item.frame")));
       }
-      $event->setCancelled(true);
-      $player->sendMessage(str_replace(["&"], ["§"], $this->config->get("no.place.item.frame")));
-    } else if ($action === PlayerInteractEvent::LEFT_CLICK_BLOCK) {
-      if ($player->hasPermission("antiframeedit.remove.bypass") or $player->isOp()) {
-        return;
-      }
-      $event->setCancelled(true);
-      $player->sendMessage(str_replace(["&"], ["§"], $this->config->get("no.remove.item.frame")));
     }
   }
 }
