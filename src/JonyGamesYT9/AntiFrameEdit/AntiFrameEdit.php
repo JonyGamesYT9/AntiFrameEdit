61bba2b6d12aa85ae5e0e95e41a68a683ffac7b6
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2021-08-26 21:35:43 -0500

    update AntiFrameEdit.php

--- src/JonyGamesYT9/AntiFrameEdit/AntiFrameEdit.php
+++ src/JonyGamesYT9/AntiFrameEdit/AntiFrameEdit.php
@@ -18,14 +18,32 @@ class AntiFrameEdit extends PluginBase implements Listener
   /** @var Config $config */
   private $config;
 
+  /** @var Config $messages */
+  private $messages;
+  
+  /** @var string[] $world */
+  public static $world;
+  
   /**
   * @return void
   */
   public function onEnable(): void
   {
     $this->saveResource("messages.yml");
-    $this->config = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
+    $this->messages = new Config($this->getDataFolder() . "messages.yml", Config::YAML);
+    $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  
+  /**
+   * @return string 
+   */
+  public function getAllWorlds(): string 
+  {
+    foreach ($this->config->get("worlds") as $worlds) {
+      self::$world[] = $worlds;
+    }
+    return self::$world ?? "";
   }
 
   /**
@@ -37,6 +55,7 @@ class AntiFrameEdit extends PluginBase implements Listener
     $player = $event->getPlayer();
     $block = $event->getBlock();
     $action = $event->getAction();
+    if ($player->getLevel()->getFolderName() === $this->getAllWorld()) {
     if ($block->getId() === Block::ITEM_FRAME_BLOCK) {
       if ($action === PlayerInteractEvent::RIGHT_CLICK_BLOCK) {
         if ($player->hasPermission("antiframeedit.place.bypass") or $player->isOp()) {
@@ -51,6 +70,7 @@ class AntiFrameEdit extends PluginBase implements Listener
         $event->setCancelled(true);
         $player->sendPopup(str_replace(["&"], ["§"], $this->config->get("no.remove.item.frame")));
       }
+    }
     }
   }
 }
